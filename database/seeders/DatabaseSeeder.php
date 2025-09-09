5b92041504def5cc8c324d855ec553fe6bcc2f21
Orelha-Gamerr <angelolucietto@gmail.com>
2025-09-09 16:55:46 -0300

    Add seeders and factories for Aluno and CategoriaAluno models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,11 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
+        $this->call([
+            AlunoSeeder::class,
+            CategoriaAlunoSeeder::class,
+        ]);
+
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
